a63db4ec7f4a57777d8eb1ef071aaf38483f9a8d
Simon Kiener <kiener.simon@gmail.com>
2011-12-01 16:15:24 +0100

    added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  
+  # Return title on a per-page basis
+  def title
+    base_title = "Rails Sample app"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
