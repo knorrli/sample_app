14fc2e8bda2d8affb097ac766fbf2cd75cdf27dd
Simon Kiener <jugglinghobo@gmail.com>
2011-12-17 05:32:41 +0100

    added Users controller and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 SampleApp::Application.routes.draw do
 
+  get "users/new"
+
   root :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match '/contact', :to => 'pages#contact'
-  
+  match '/signup', :to => 'users#new'  
   
   get "pages/home"
 
--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -12,13 +12,35 @@ describe "LayoutLinks" do
       response.should have_selector('title', :content => "Contact")
     end
     
-     it "should have About page at /about" do
-        get '/about'
-        response.should have_selector('title', :content => "About")
-      end
+    it "should have About page at /about" do
+      get '/about'
+      response.should have_selector('title', :content => "About")
+    end
+    
+    it "should have Help page at /help" do
+      get '/help'
+      response.should have_selector('title', :content => "Help")
+    end
+    
+    it "should have Signup page at /signup" do
+      get '/signup'
+      response.should have_selector('title', :content => "Sign Up")
+    end
+    
+    it "should have the right links on the layout" do
+      visit root_path
+      response.should have_selector('title', :content => "Home")
+      click_link "About"
+      response.should have_selector('title', :content => "About")
+      click_link "Contact"
+      response.should have_selector('title', :content => "Contact")
+      click_link "Help"
+      response.should have_selector('title', :content => "Help")
+      click_link "Home"
+      response.should have_selector('title', :content => "Home")
+      click_link "Sign Up"
+      response.should have_selector('title', :content => "Sign Up")
       
-       it "should have Help page at /help" do
-          get '/help'
-          response.should have_selector('title', :content => "Help")
-        end
+      response.should have_selector('a[href="/"]>img')
+    end
 end
